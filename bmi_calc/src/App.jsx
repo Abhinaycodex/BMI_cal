00b40c7170779e63bbf546d1ba89bbf4f279bbe9
Abhinay Raj Gupta <132003064+Abhinaycodex@users.noreply.github.com>
2024-08-02 00:51:11 +0530

    done work

--- bmi_calc/src/App.jsx
+++ bmi_calc/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Height, setHeight] = useState(0);
+  const [Weight, setWeight] = useState(0);
+  const [Result, setResult] = useState("");
+  const [BMI, setBMI] = useState("");
+
+  let cal = (e) => {
+    e.preventDefault();
+
+    if(Weight===0 || Height===0){
+      alert("Please enter both height and weight")
+    }
+    else{
+      let bmi = (Weight / (Height * Height)) * 10000;
+      setBMI(bmi.toFixed(1))
+      if (bmi < 18.5) {
+        setResult("Underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
+          setResult("healthy");
+          }
+        else if (bmi >= 25 && bmi < 30) {
+          setResult("Overweight");
+          }
+        else {
+          setResult("Obese");
+        }
+    }
+
+  }
+  let reload = ()=>{
+    window.location.reload()
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="container">
+          <h1>BMI CALCULATOR</h1>
+
+          <form onSubmit={cal}>
+          <div>
+            <label>Height (cm):</label>
+            <input value={Height} onChange={(e) => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Weight (kg):</label>
+            <input value={Weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <button className="btn" type="submit">Calculate</button>
+          <button className="btn btn-outline" onClick={reload} type="submit">Reload</button>
+            
+          </form>
+          <div className="center">
+            <h3>your BMI is : {BMI}</h3>
+            <h3>your status is : {Result}</h3>
+          </div>
+
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
